f4dfb8cccb8877ddb7f5e90643b8e0063f33fa30
Megs <Megs.close@gmail.com>
2021-03-02 18:58:08 -0800

    capturing data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,10 +4,29 @@ var getUserRepos = function(user) {
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+        console.log(response);
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-getUserRepos("microsoft");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element (trim helps if there's an accidental space)
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        //after passing data as argument, clear the form
+        nameInputEl.value="";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
